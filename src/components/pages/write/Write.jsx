a4713eeda1bc04c4db4495fb16dcbd3e7cc9917c
zijie <liangzijie1437@gmail.com>
2022-05-15 15:48:57 +0200

    Add static image in write page

--- src/components/pages/write/Write.jsx
+++ src/components/pages/write/Write.jsx
@@ -1,21 +1,29 @@
-import './write.css'
+import "./write.css";
 
 export default function Write() {
   return (
-    <div className='write'>
-        <form className="writeForm">
-            <div className="writeFormGroup">
-                <label htmlFor="fileInput">
-                    <i className="writeIcon fa-solid fa-plus"></i>
-                </label>
-                <input type="file" id='fileInput' style={{display: "none"}}/>
-                <input type="text"  className='writeInput' autoFocus={true}/>
-            </div>
-            <div className="writeFormGroup">
-                <textarea className='writeInput writeText' placeholder='Tell your story ...'></textarea>
-            </div>
-            <button className="writeSubmit">Publish</button>
-        </form>
+    <div className="write">
+      <img
+        src="https://images.unsplash.com/photo-1465447142348-e9952c393450?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
+        alt=""
+        className="writeImg"
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input type="text" className="writeInput" autoFocus={true} />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story ..."
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
     </div>
-  )
+  );
 }
