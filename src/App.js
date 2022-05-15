e581dcde26383c3bfe0caa507c387b8f7566a8ed
zijie <liangzijie1437@gmail.com>
2022-05-15 08:53:22 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/header/Header';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Header/>
     </div>
   );
 }
